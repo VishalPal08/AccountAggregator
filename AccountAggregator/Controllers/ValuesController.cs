8399c111aa036c6f1d77ef668abec5bf43966cb8
AvadhutParab <avadhut.parab@sudlife.in>
2022-09-16 15:59:02 +0530

    Remove comment from value controller

--- AccountAggregator/Controllers/ValuesController.cs
+++ AccountAggregator/Controllers/ValuesController.cs
@@ -14,14 +14,14 @@ namespace AccountAggregator.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+       
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+       
         [HttpPost]
         public void Post([FromBody] string value)
         {
